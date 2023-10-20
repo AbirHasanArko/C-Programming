2d861b68804ab095d865f97effcb8e6f95b36ad2
Abir Hasan Arko <140732716+AbirHasanArko@users.noreply.github.com>
2023-10-21 02:53:00 +0600

    Update Power-Function.c
    C program to find "Power of a Number" with User-Defined Function

--- LibraryFunction-to-UserDefinedFunction/Power-Function.c
+++ LibraryFunction-to-UserDefinedFunction/Power-Function.c
@@ -26,12 +26,12 @@ float power(float base, float exponent){
         return 1.0 / power(base, - exponent);
     }
     /* if 'x^-y' is an expression, where the exponent is '0':
-    (Any Real or Complex number) ^ 0 = 1 */
+    (Any Real or Complex number)^0 = 1 */
     else if(exponent == 0){
         return 1.0;
     }
     /* if 'x^-y' is an expression, where x,y ∈ R:
-    x^y = x*x*X*...... (y times)  */
+    x^y = x*x*x*...... (y times)  */
     else{
         float result = 1.0;
         for(int i = 1; i <= exponent; i++){
@@ -41,19 +41,22 @@ float power(float base, float exponent){
     }
 }
 
-/*---------------------------------------- Additional Information ----------------------------------------*/
+
+/*--------------------------------------------- Additional Information ---------------------------------------------*/
 /*
 Library Function: pow(base, exponent);
 Header File: <math.h>
-----------------------------------------------------------------------------------------------------------
-Description: Given two numbers base and exponent, the C++or C pow() function finds x raised to the power of y i.e. x^y. Basically in C/C++, the exponent value is calculated using the pow() function. The pow() function is used to calculate the power of a number in C/C++. It takes double as input and returns double as output.
+----------------------------------------------------------------------------------------------------------------------
+Description: Given two numbers base and exponent, the C++or C pow() function finds x raised to the power of y i.e. x^y. 
+Basically in C/C++, the exponent value is calculated using the pow() function. The pow() function is used to calculate 
+the power of a number in C/C++. It takes double as input and returns double as output.
 We have to use #include<math.h> in C/C++  to use that pow() function in our C/C++ program.
 
 This method takes only two arguments:
 x: floating point base value
 y: floating point power value
 The power function returns the floating point value of x raised to the power y ( x^y ).
-----------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
 Credit of Description: https://www.geeksforgeeks.org/power-function-c-cpp/
 */
   
